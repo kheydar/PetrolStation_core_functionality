464b995ed86c237fd0dd239c7f2f84f7e5e83caf
kheydar <nelsonrodrigues@hotmail.co.uk>
2022-11-18 12:38:49 +0000

    new code

--- PetrolStation_core_functionality/Program.cs
+++ PetrolStation_core_functionality/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Timers;
-using Timer = System.Timers.Timer;
 
 namespace PetrolStation
 {
     public class Program
     {
-        
-        static int newVehicle = 1500;
-        static int refuelTime = 8000;
-        static double litresDispensed = 0;
-        static int avaliablePumps = 9;
-        static int carsCreated = 0;
-        static int carsServed = 0;
-        static bool running = true;
+
+        private static int newVehicle = 1500;
+        private static int refuelTime = 8000;
+        private static double litresDispensed = 0;
+        private static int avaliablePumps = 9;
+        private static int carsCreated = 0;
+        private static int carsServed = 0;
+        private static bool running = true;
+        private static int carsQueue = 0;
+        private static int carsLeftEarly = 0;
 
         private static System.Timers.Timer createVechile;
         private static System.Timers.Timer refuel;
@@ -29,7 +30,20 @@ namespace PetrolStation
                 string userInput = Console.ReadLine();
                 avaliablePumps -= 1;
                 Console.WriteLine($"Vechile assigned to pump {userInput}, {avaliablePumps} pumps avaliable");
-                
+                Console.WriteLine("\n");
+                Console.WriteLine(new string('#', 100));
+                //Console.WriteLine("\n");
+                //Console.Clear();
+
+                Console.WriteLine($"Queue: {carsQueue} \n" +
+                    $"Cars: {carsCreated} \n"+
+                    $"Litres sold: {litresDispensed} \n" +
+                    $"Cost: \n" +
+                    $"1%: \n" +
+                    $"Vehicles serviced: {carsServed} \n" +
+                    $"Avaliable pumps: {avaliablePumps} \n" +
+                    $"Left early: \n");
+
             } while (avaliablePumps > 0);
         }
 
@@ -45,7 +59,7 @@ namespace PetrolStation
 
         private static void Refuel()
         {
-            refuel = new Timer(refuelTime);
+            refuel = new System.Timers.Timer(refuelTime);
             refuel.Enabled = true;
             refuel.AutoReset = true;
             refuel.Elapsed += RefuelTimer;
@@ -56,6 +70,7 @@ namespace PetrolStation
         private static void VehicleTimer(Object source, ElapsedEventArgs e)
         {
             carsCreated += 1;
+            carsQueue += 1;
         }
 
         private static void RefuelTimer(Object source, ElapsedEventArgs e)
@@ -64,7 +79,8 @@ namespace PetrolStation
             litresDispensed += fuelDispensed;
             carsServed += 1;
             avaliablePumps += 1;
-            Console.WriteLine($"{carsServed} cars served; {litresDispensed} litres of fuel sold, {avaliablePumps} pumps avaliable");
+            carsQueue -= 1;
+            //Console.WriteLine($"{carsServed} cars served; {litresDispensed} litres of fuel sold, {avaliablePumps} pumps avaliable");
         }
 
     }
