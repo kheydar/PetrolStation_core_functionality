6ad657e6fbacd76050ccab47f4ed4b8389665e56
kheydar <nelsonrodrigues@hotmail.co.uk>
2022-11-21 11:46:11 +0000

    Added login screen

--- PetrolStation_core_functionality/Program.cs
+++ PetrolStation_core_functionality/Program.cs
@@ -14,6 +14,7 @@ namespace PetrolStation
         private static int carsCreated = 0;
         private static int carsServed = 0;
         private static bool running = true;
+        private static bool correctPin;
         private static int carsQueue = 0;
         private static int carsLeftEarly = 0;
         private static double cost;
@@ -24,46 +25,88 @@ namespace PetrolStation
 
         static void Main(string[] args)
         {
-            VechileTimer();
-            Refuel();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            
+            Console.WriteLine("Welcome to Broken Petrol Ltd");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Please login to continue");
+            string login = Console.ReadLine();
 
-            do
+            if (login =="123")
             {
-                while (avaliablePumps==0)
+                correctPin = true;
+            }
+
+            else
+            {
+                correctPin = false;
+            }
+
+            if (!correctPin)
+            {
+                do
                 {
-                    Console.WriteLine("Waiting for a free pump...");
-                    Thread.Sleep(2000);
-                }
+                   
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Incorrect pin, please try again...");
+                    string input = Console.ReadLine();
+                    if (input == "123")
+                    {
+                        correctPin = true;
+                    }
+                    
+                } while (!correctPin);
+            }
 
-                Console.WriteLine("New vehicle arrived, please select pump or type 'quit' to close the program'");
-                string userInput = Console.ReadLine();
-
-                if (userInput.ToLower() == "quit")
+            if (correctPin)
                 {
-                    running = false;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Log in successful");
+                    VechileTimer();
+                    Refuel();
+
+                    do
+                    {
+                        while (avaliablePumps==0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Waiting for a free pump...");
+                            Thread.Sleep(2000);
+                            Console.WriteLine("\n");
+                        }
+
+                        Console.ResetColor();
+                        Console.WriteLine("New vehicle arrived, please select pump or type 'quit' to close the program'");
+                        string userInput = Console.ReadLine();
+
+                        if (userInput.ToLower() == "quit")
+                        {
+                            running = false;
+                        }
+
+                        avaliablePumps -= 1;
+                        Console.WriteLine("\n");
+                        Console.WriteLine($"Vechile assigned to pump {userInput}, {avaliablePumps} pumps avaliable");
+                        Console.WriteLine("\n");
+                        Console.WriteLine(new string('#', 100));
+                        //Console.Clear();
+
+                        Console.WriteLine($"Queue: {carsQueue} \n" +
+                            $"Cars: {carsCreated} \n"+
+                            $"Litres sold: {litresDispensed} \n" +
+                            $"Cost: £ {cost}\n" +
+                            $"1%: £ {commision}\n" +
+                            $"Vehicles serviced: {carsServed} \n" +
+                            $"Avaliable pumps: {avaliablePumps} \n"
+                            //+$"Left early: \n"
+                            );
+
+
+                    } while (running);
                 }
-
-                avaliablePumps -= 1;
-                Console.WriteLine("\n");
-                Console.WriteLine($"Vechile assigned to pump {userInput}, {avaliablePumps} pumps avaliable");
-                Console.WriteLine("\n");
-                Console.WriteLine(new string('#', 100));
-                //Console.Clear();
-
-                Console.WriteLine($"Queue: {carsQueue} \n" +
-                    $"Cars: {carsCreated} \n"+
-                    $"Litres sold: {litresDispensed} \n" +
-                    $"Cost: £{cost}\n" +
-                    $"1%: £{commision}\n" +
-                    $"Vehicles serviced: {carsServed} \n" +
-                    $"Avaliable pumps: {avaliablePumps} \n"
-                    //+$"Left early: \n"
-                    );
-
-
-            } while (running);
-        }
-
+            }   
 
         private static void VechileTimer()
         {
@@ -101,6 +144,6 @@ namespace PetrolStation
             carsQueue -= 1;
         }
 
-    }
 
+    }
 }
